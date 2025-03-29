5e7d76b228e5a1d9d893a8560d6c56f10c137e98
Tekfa96 <aymen.tekfa96@gmail.com>
2025-03-29 01:59:24 +0100

    test api

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,13 +1,30 @@
 import Agent from "@/app/components/Agent";
+import { getCurrentUser } from "@/lib/actions/auth.action";
 import React from "react";
 
-const page = () => {
+const page = async () => {
+  const user = await getCurrentUser();
   return (
     <>
       <h3>Interiew generation</h3>
-      <Agent userName="You" userId="user1" type="generate" />
+      <Agent userName={user?.name} userId={user?.id} type="generate" />
     </>
   );
 };
 
 export default page;
+
+// STATIC version
+// import Agent from "@/app/components/Agent";
+// import React from "react";
+
+// const page = () => {
+//   return (
+//     <>
+//       <h3>Interiew generation</h3>
+//       <Agent userName="You" userId="user1" type="generate" />
+//     </>
+//   );
+// };
+
+// export default page;
